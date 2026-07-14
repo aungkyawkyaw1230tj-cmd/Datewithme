82d44540340c3a5cc5b5b2f3793b0ac63eb223e0
aungkyawkyaw1230tj-cmd <aungkyawkyaw1230tj@gmail.com>
2026-07-14 12:20:41 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,13 @@
+from flask import Flask, render_template
 import os
-from flask import Flask, render_template, jsonify
-import requests
 
 app = Flask(__name__)
 
-# Render Environment Variables
-RAPID_KEY = os.getenv('RAPIDAPI_KEY')
-PANDA_TOKEN = os.getenv('PANDASCORE_TOKEN')
-
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/api/football-matches')
-def get_football():
-    today = "2026-07-14" 
-    url = f"https://sportapi7.p.rapidapi.com/api/v1/category/1/scheduled-events/{today}"
-    headers = {"x-rapidapi-key": RAPID_KEY, "x-rapidapi-host": "sportapi7.p.rapidapi.com"}
-    response = requests.get(url, headers=headers)
-    return jsonify(response.json())
-
-@app.route('/')
-def index():
-    return "<h1>Hello! Flask is working.</h1>"
+    return "<h1>System is working!</h1>"
 
 if __name__ == '__main__':
-    app.run()
-
+    # Render အတွက် Port ပေးဖို့ လိုနိုင်ပါတယ်
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
