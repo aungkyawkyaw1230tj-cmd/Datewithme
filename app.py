f764f81776a943b6a0168324277e6bda571480c2
aungkyawkyaw1230tj-cmd <aungkyawkyaw1230tj@gmail.com>
2026-07-15 12:29:56 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-from flask import Flask, render_template
-import traceback
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    try:
-        return render_template('index.html')
-    except Exception as e:
-        return str(traceback.format_exc()) # Error တက်ရင် ဘာဖြစ်လို့လဲဆိုတာ Browser ပေါ်မှာ တန်းပြလိမ့်မယ်
